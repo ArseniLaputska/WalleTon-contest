3deba0f0b85624fa687d04b7d8eb74d0546efa9a
Arseni <arsenii12345@yandex.ru>
2023-03-23 16:31:02 +0300

    init commit

---
+++ WalleTon/WalleTon/WalleTonApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalleTonApp.swift
+//  WalleTon
+//
+//  Created by Arseni Laputska on 23.03.23.
+//
+
+import SwiftUI
+
+@main
+struct WalleTonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
