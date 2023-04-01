73992acf29621fa00164c2044e7e26caa6aecf4f
Arseni <arsenii12345@yandex.ru>
2023-04-01 18:05:16 +0300

    added alerts to recovery phrases

--- WalleTon/WalleTon/CreateWallet/RecoveryPhraseView.swift
+++ WalleTon/WalleTon/CreateWallet/RecoveryPhraseView.swift
@@ -11,6 +11,9 @@ struct RecoveryPhraseView: View {
     
     let phrases: [String] = ["network","network","network","network","network","network","network","network","network","network","network","network","network","network","network","network","network","network","network","network","network","network","network","network","network","network","network","network","network"]
     
+    @State private var showAlert: Bool = false
+    @State private var secondClick: Bool = false
+    
     var body: some View {
         ScrollView {
             ZStack {
@@ -32,6 +35,25 @@ struct RecoveryPhraseView: View {
                         .padding(.bottom, 52.0)
                     
                     MainButton(label: "Done", action: {
+                        showAlert.toggle()
+                        secondClick.toggle()
+                        
+                    }).alert(isPresented: $showAlert, content: {
+                        let titleView = Text("Sure done?").bold()
+                        
+                        let messageView = Text("You didn't have enough time to write these words down.")
+                        
+                        let primaryButtonTitle = Text("OK, sorry").bold()
+                        let primaryButton = Alert.Button.default(primaryButtonTitle)
+                        
+                        let secondaryButtonTitle = Text("Skip")
+                        let secondaryButton = Alert.Button.default(secondaryButtonTitle)
+                        
+                        if secondClick {
+                            return Alert(title: titleView, message: messageView, dismissButton: primaryButton)
+                        } else {
+                            return Alert(title: titleView, message: messageView, primaryButton: primaryButton, secondaryButton: secondaryButton)
+                        }
                         
                     })
                     
