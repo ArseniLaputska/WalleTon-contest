80278c01d603514c8cbae751225c4b3b97b08d1e
Arseni <arsenii12345@yandex.ru>
2023-04-11 01:48:41 +0300

    added scroll view to main view

--- WalleTon/WalleTon/Wallet/MainLoading.swift
+++ WalleTon/WalleTon/Wallet/MainLoading.swift
@@ -14,40 +14,74 @@ enum MainState {
 }
 
 struct MainLoading: View {
+    
+    @State private var headerOffset: CGFloat = 0
+    @State private var mainState: MainState = .loading
+    
     var body: some View {
-        ZStack(alignment: .top) {
+        ZStack {
             VStack {
-                VStack {
-
-                    MainNavigationView(scan: {
-                        
-                    }, settings: {
-                        
-                    })
+                
+                MainNavigationView(scan: {
                     
-                    AnimationView(sticker: .main)
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 36, height: 36)
-                        .padding(.top, 52.0)
+                }, settings: {
                     
-                    MainButtonsView(receive: {
+                })
+                .background(Color.black)
+                    
+                ScrollView {
+                    VStack {
+                        AnimationView(sticker: .main)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 36, height: 36)
+                            .padding(.top, 52.0)
+                            .background(Color.black)
                         
-                    }, send: {
-                        
-                    })
-                    .padding(.top, 72.0)
-                    .padding(.bottom, 16.0)
+                        MainButtonsView(receive: {
+                            
+                        }, send: {
+                            
+                        })
+                        .frame(height: 50)
+                        .padding(.top, 72.0)
+                        .padding(.bottom, 34.0)
+                        .background(Color.black)
+                    }
+                    .background(Color.black)
                     
                     VStack {
-                        AnimationView(sticker: .loading)
+                        switch mainState {
+                            case .created:
+                                AnimationView(sticker: .loading)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 124, height: 124)
+                                    .padding(.top, 170.0)
+                            case .loading:
+                                // todo
+                                AnimationView(sticker: .loading)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 124, height: 124)
+                                    .padding(.top, 170.0)
+                            case .transactions:
+                                    //to do
+                                AnimationView(sticker: .loading)
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 124, height: 124)
+                                    .padding(.top, 170.0)
+                        }
                     }
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
                     .background(Color.white)
-                    .cornerRadius(15.0)
+                    .cornerRadius(12.0)
+                    .padding(.top, -18.0)
+                    
                 }
+                .background(Color.white)
+                .cornerRadius(12.0)
             }
             .background(Color.black)
-            
         }
+        .background(Color.white)
     }
 }
 
