377dd354d46f30e832476a69561f0b9d06afb3e1
Arseni <arsenii12345@yandex.ru>
2023-04-13 16:59:39 +0300

    added User model

---
+++ WalleTon/WalleTon/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  WalleTon
+//
+//  Created by Arseni Laputska on 13.04.23.
+//
+
+import Foundation
+
+struct User: Decodable {
+    let wallet: String
+    let balance: Double
+}
